31cb8fd18ed8667207ca8cd2877fbc66f1c8ccbd
Charlie Bevis <charliebevis@gmail.com>
2017-09-26 14:17:23 -0500

    initial commit. Following https://www.yesodweb.com/book/creating-a-subsite

---
+++ src/HelloSub.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+module HelloSub
+    ( module HelloSub.Data
+    , module HelloSub
+    ) where
+
+import           HelloSub.Data
+import           Yesod
+
+-- And we'll spell out the handler type signature.
+getSubHomeR :: Yesod master => HandlerT HelloSub (HandlerT master IO) Html
+getSubHomeR = lift $ defaultLayout [whamlet|Welcome to the subsite!|]
+
+instance Yesod master => YesodSubDispatch HelloSub (HandlerT master IO) where
+    yesodSubDispatch = $(mkYesodSubDispatch resourcesHelloSub)
---
+++ src/HelloSub/Data.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE QuasiQuotes     #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
+module HelloSub.Data where
+
+import           Yesod
+
+-- Subsites have foundations just like master sites.
+data HelloSub = HelloSub
+
+-- We have a familiar analogue from mkYesod, with just one extra parameter.
+-- We'll discuss that later.
+mkYesodSubData "HelloSub" [parseRoutes|
+/ SubHomeR GET
+|]
